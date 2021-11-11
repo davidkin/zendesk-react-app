33e8fe9343bde21f88e7c608f4c0322ef4fe13bc
David Potapkin <david.potapkin@dataart.com>
2021-11-11 19:15:50 +0200

    init commit

--- src/EntryView.tsx
+++ src/EntryView.tsx
@@ -1,61 +1,41 @@
 import * as React from 'react'
-import {
-  useClientHeight,
-  ResponseHandler,
-  useSellContactEmail,
-  useFormattedDate,
-} from '@zendesk/sell-zaf-app-toolbox'
+// tslint:disable-next-line:origin-ordered-imports
 import {Row, Col, Grid} from '@zendeskgarden/react-grid'
-import ZendeskStroke from '@zendeskgarden/svg-icons/src/16/zendesk-stroke.svg'
-
-import Loader from './components/Loader'
-import EmptyStateSVG from './svg/EmptyState.svg'
+import {ORDERS_MOCK} from './mock'
 
 import css from './App.css'
 
-const Header = ({contactEmail}: {contactEmail: string}) => (
-  <Row justifyContent="center" className={css.header}>
-    <div className={css.icon}>
-      <ZendeskStroke />
-    </div>
-    <div className={css.title}>
-      What are you looking for <strong>{contactEmail}</strong>?
-    </div>
-  </Row>
-)
-
-const ContentView = () => {
-  return (
-    <Row justifyContent="center" className={css.contentView}>
-      <EmptyStateSVG width={'40%'} />
-    </Row>
-  )
-}
-
 export const EntryView = () => {
-  useClientHeight(230)
-  const contactEmailResponse = useSellContactEmail()
-  const todayDate = useFormattedDate(new Date())
-
   return (
-    <Grid gutters={false} className={css.App}>
-      <Row>
-        <ResponseHandler
-          response={contactEmailResponse}
-          loadingView={<Loader />}
-          errorView={<div>Something went wrong!</div>}
-          emptyView={<div>There's nothing to see yet.</div>}
-        >
-          {([sellContact]: [string]) => (
-            <Col textAlign="center">
-              <Header contactEmail={sellContact} />
-              <div className={css.dateContainer}>Date: {todayDate}</div>
-              <ContentView />
+    <div>
+      <div className={css.mainText}>
+        <p>Host name is David.</p>
+      </div>
+      <Grid debug>
+        {ORDERS_MOCK.map((item) => (
+          <Row justifyContent="center" key={item.cart_id}>
+            <Col size={4}>
+              <h3>Cart Id:</h3>
+              <div>{item.cart_id}</div>
             </Col>
-          )}
-        </ResponseHandler>
-      </Row>
-    </Grid>
+            <Col size={4}>
+              <h3>
+                <b>Billing Address</b>
+              </h3>
+              <div>Email: {item.billing_address.email} </div>
+              <div>
+                Full name:{' '}
+                {`${item.billing_address.first_name} ${item.billing_address.last_name}`}
+              </div>
+              <div>City: {item.billing_address.city}</div>
+              {item.billing_address.phone && (
+                <div>Phone: {item.billing_address.phone}</div>
+              )}
+            </Col>
+          </Row>
+        ))}
+      </Grid>
+    </div>
   )
 }
 
