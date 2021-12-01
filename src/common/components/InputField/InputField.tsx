d1d3351e05c5495341e5958e6022bc6d47af3423
David Potapkin <david.potapkin@dataart.com>
2021-12-01 20:42:40 +0200

    feature/markup-search-PAPER-3160: create basic layout for search field and home page

---
+++ src/common/components/InputField/InputField.tsx
@@ -0,0 +1,23 @@
+import React, {FC} from 'react';
+import {Field, Input, Label} from '@zendeskgarden/react-forms';
+import styles from './InputField.css';
+
+interface IProps {
+  className?: string;
+  type: string;
+  onChange: () => any;
+  placeholder: string;
+  readOnly?: boolean;
+  label?: string;
+}
+
+const InputField: FC<IProps> = ({placeholder, label}) => {
+  return (
+    <Field className={styles.field}>
+      {label && <Label>{label}</Label>}
+      <Input placeholder={placeholder} />
+    </Field>
+  );
+};
+
+export default InputField;
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,17 +1,8 @@
-import React, {FC, useCallback, useContext, useMemo} from 'react';
-import {Row, Col, Grid} from '@zendeskgarden/react-grid';
-import {ZAFClientContext} from '@zendesk/sell-zaf-app-toolbox';
+import React, {FC} from 'react';
 import algoliasearch from 'algoliasearch';
 import {InstantSearch, SearchBox, Hits} from 'react-instantsearch-dom';
-import {ORDERS_MOCK} from '../../mock';
-import {GENERATE_ZENDESK_URL} from '../../environment';
-import {useOrderContext} from '../../common/context/OrderContext';
-import {ZafClientData} from '../../common/types';
-import {IOrder} from '../../common/interfaces';
-import Order from '../../common/components/Order/Order';
+import {Row, Col} from '@zendeskgarden/react-grid';
 import styles from './HomePage.css';
-
-type Order = IOrder | undefined;
 
 const searchClient = algoliasearch(
   'I67M45588W',
@@ -19,95 +10,21 @@ const searchClient = algoliasearch(
 );
 
 export const HomePage: FC = () => {
-  const {orderId} = useOrderContext();
-  const data: ZafClientData | undefined = useContext(ZAFClientContext);
-
-  const currentOrder: Order = useMemo(
-    () =>
-      orderId
-        ? ORDERS_MOCK.find((item) => item.cart_id === orderId)
-        : undefined,
-    [orderId],
-  );
-
-  const handleRoute = useCallback(
-    (cartId: string) => {
-      if (!data?.account) {
-        return;
-      }
-
-      const url = GENERATE_ZENDESK_URL(data.account.subdomain);
-
-      const params =
-        data?.appParams && data.appParams.zcli_apps ? '?zcli_apps=true&' : '?';
-
-      window.open(`${url}${params}orderId=${cartId}`, '_blank', 'noopener');
-    },
-    [data],
-  );
-
   return (
-    <div>
-      <div className={styles.searchBlock}>
-        <InstantSearch searchClient={searchClient} indexName="dev_tk">
-          <SearchBox />
-          <Hits hitComponent={Order} />
-        </InstantSearch>
-      </div>
+    <div className={styles.App}>
+      <InstantSearch searchClient={searchClient} indexName="dev_tk">
+        <Row justifyContent="center">
+          <Col md={12}>
+            <SearchBox
+              autoFocus
+              className={styles.searchField}
+              translations={{placeholder: 'Search'}}
+            />
+          </Col>
+        </Row>
 
-      <Grid debug>
-        {!currentOrder ? (
-          ORDERS_MOCK.map((item) => (
-            <Row key={item.cart_id} className={styles.card}>
-              <Col md={6} offsetMd={3} sm={12}>
-                <h3>
-                  Order: <b>#{item.cart_id.split('-')[0].toUpperCase()}</b>
-                </h3>
-                <div>
-                  <b>Customer name: </b>
-                  {`${item.billing_address.first_name} ${item.billing_address.last_name}`}
-                </div>
-                <div>
-                  <b>Wedding date: </b>
-                  {item.date_created}
-                </div>
-                <div>
-                  <button onClick={() => handleRoute(item.cart_id)}>
-                    Check Order
-                  </button>
-                </div>
-              </Col>
-            </Row>
-          ))
-        ) : (
-          <Row>
-            <Col md={6} offsetMd={3}>
-              <h3>Order #{currentOrder.cart_id.split('-')[0].toUpperCase()}</h3>
-              <div>
-                <b>Customer name: </b>
-                {`${currentOrder.billing_address.first_name} ${currentOrder.billing_address.last_name}`}
-              </div>
-              <div>
-                <b>Wedding date: </b>
-                {currentOrder.date_created}
-              </div>
-
-              <div className={styles.infoBlock}>
-                <h3>Order activity: </h3>
-                <hr />
-              </div>
-              <div className={styles.infoBlock}>
-                <h3>Payments: </h3>
-                <hr />
-              </div>
-              <div className={styles.infoBlock}>
-                <h3>Shipments: </h3>
-                <hr />
-              </div>
-            </Col>
-          </Row>
-        )}
-      </Grid>
+        <div className={styles.hitsBlock}><Hits /></div>
+      </InstantSearch>
     </div>
   );
 };
